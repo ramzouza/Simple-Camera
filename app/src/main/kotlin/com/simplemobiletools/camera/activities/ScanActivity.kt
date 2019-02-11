8e9149e6c6782070fce1162b4cbcb0cf90408d01
Yann Cedric <ngadeuyann@yahoo.fr>
2019-02-10 19:57:22 -0500

    finished the qr scanner feature. Users can now follow the qr code or cancel the request
    Co-authored-by: Ribal Aladeeb <aladeeb.r@hotmail.com>
    Co-authored-by: ramzouza <ramezzaid@outlook.com>

--- app/src/main/kotlin/com/simplemobiletools/camera/activities/ScanActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/camera/activities/ScanActivity.kt
@@ -1,12 +1,19 @@
 package com.simplemobiletools.camera.activities
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.view.*
 import com.simplemobiletools.camera.R
 import kotlinx.android.synthetic.main.activity_main.*
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
+
 
 class ScanActivity : SimpleActivity(), ZXingScannerView.ResultHandler{
     private var mScannerView: ZXingScannerView? = null
@@ -28,21 +35,60 @@ class ScanActivity : SimpleActivity(), ZXingScannerView.ResultHandler{
         mScannerView!!.stopCamera()           // Stop camera on pause
     }
 
+    fun navigate(url: String){
+        val openURL = Intent(Intent.ACTION_VIEW)
+        openURL.data = Uri.parse(url)
+        startActivity(openURL)
+    }
+
+    fun cancel(dialog : DialogInterface){
+        dialog.cancel()
+        mScannerView!!.resumeCameraPreview(this)
+    }
+
     override fun handleResult(rawResult: Result) {
         // Do something with the result here
         // Log.v("tag", rawResult.getText()); // Prints scan results
         // Log.v("tag", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+//
+//        var view = LayoutInflater.from().inflate(R.layout.activity_main, null)
+//        var button = view.findViewById<Button>(R.id.smart_notif)
+//
+//
+//        println("Result")
+//        println(button)
+//        println(rawResult.text)
+//
+//
+//        button.text = rawResult.text
+//
+//        println("Done")
 
+        // build alert dialog
+        val dialogBuilder = AlertDialog.Builder(this, AlertDialog.THEME_HOLO_LIGHT)
 
-        println(smart_notif)
+        // set message of alert dialog
+        dialogBuilder.setTitle("Link Found")
+                .setMessage("Do you want to visit this link: "+ rawResult.text +" ?")
+                // if the dialog is cancelable
+                .setCancelable(true)
+                // positive button text and action
+                .setPositiveButton("Proceed", DialogInterface.OnClickListener {
+                    dialog, id -> navigate(rawResult.text)
+                })
+                // negative button text and action
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+                    dialog, id -> cancel(dialog)
+                })
 
-        println("Result")
-        println(rawResult.text)
-
-        smart_notif.setText(rawResult.text)
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        // show alert dialog
+        alert.show()
 
         // If you would like to resume scanning, call this method below:
-        //mScannerView.resumeCameraPreview(this);
+//         mScannerView!!.resumeCameraPreview(this);
     }
 }
 
