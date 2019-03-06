75c3b4565e8357f9df20bdae63b35a97618af11e
Sibachir Ahmed-Yanis <ahmed.yanis.sibachir@gmail.com>
2019-03-05 22:08:15 -0500

    Create Filter List class with handlers #13

---
+++ app/src/main/kotlin/com/simplemobiletools/camera/activities/FilterList.kt
@@ -0,0 +1,76 @@
+package com.simplemobiletools.camera.activities
+
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import android.util.Log
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.simplemobiletools.camera.Adapter.ThumbnailAdapter
+import com.simplemobiletools.camera.R
+import com.simplemobiletools.camera.interfaces.FilterListInterface
+import com.simplemobiletools.camera.Utils.BitmapTools
+import com.simplemobiletools.camera.Utils.SpaceItemDecoration
+import com.zomato.photofilters.FilterPack
+import com.zomato.photofilters.imageprocessors.Filter
+import com.zomato.photofilters.utils.ThumbnailItem
+import com.zomato.photofilters.utils.ThumbnailsManager
+import kotlinx.android.synthetic.main.filter_list.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class FilterList : Fragment(),FilterListInterface {
+    internal lateinit var recycler_view: RecyclerView
+    internal var listener : FilterListInterface ?=null
+    internal lateinit var adapter:ThumbnailAdapter
+    internal lateinit var thumbnailItemList: MutableList<ThumbnailItem>
+
+    fun setListener(listFragmentListener: FilterListInterface){
+        this.listener = listFragmentListener
+    }
+
+    override fun onFilterSelected(filter: Filter) {
+        if(listener != null)
+            listener!!.onFilterSelected(filter)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val itemView = inflater.inflate(R.layout.filter_list, container, false)
+
+        thumbnailItemList = ArrayList()
+        adapter = ThumbnailAdapter((activity as Context?)!!,thumbnailItemList,this)
+
+        recycler_view = itemView.findViewById<RecyclerView>(R.id.recycler_view)
+        recycler_view.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL,false)
+        recycler_view.itemAnimator = DefaultItemAnimator()
+        val space = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,8f,resources.displayMetrics).toInt()
+        recycler_view.addItemDecoration(SpaceItemDecoration(space))
+        recycler_view.adapter = adapter
+
+        displayImage(null,Uri.parse("flash.jpg"))
+
+        return itemView
+    }
+    
+
+
+}
