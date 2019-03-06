7d1c85eca5f5cee45bd6d77e53e717af7945c2ac
Skander Bm <skander96@hotmail.com>
2019-03-05 22:47:53 -0500

    Added NonSwipeableViewPager and Methods for the interface #13

---
+++ app/src/main/kotlin/com/simplemobiletools/camera/Utils/NonSwipeableViewPager.kt
@@ -0,0 +1,47 @@
+package com.simplemobiletools.camera.Utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.animation.DecelerateInterpolator
+import android.widget.Scroller
+import androidx.viewpager.widget.ViewPager
+import dalvik.system.DexClassLoader
+import java.lang.Exception
+
+class NonSwipeableViewPager: ViewPager {
+    constructor(context:Context):super(context){
+        setMyScroller()
+
+    }
+    constructor(context:Context,AttributeSet:AttributeSet):super(context,AttributeSet){
+        setMyScroller()
+
+    }
+
+    override fun onInterceptHoverEvent(event: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        return false
+    }
+
+    private fun setMyScroller() {
+        try{
+            val viewPager = ViewPager::class.java
+            val scroller = viewPager.getDeclaredField("mScroller")
+            scroller.isAccessible = true
+            scroller.set(this,MyScroller(context))
+
+        }catch(e:Exception){
+            e.printStackTrace()
+        }
+    }
+}
+
+class MyScroller(context: Context):Scroller(context,DecelerateInterpolator()){
+    override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+        super.startScroll(startX, startY, dx, dy, 400)
+    }
+}
