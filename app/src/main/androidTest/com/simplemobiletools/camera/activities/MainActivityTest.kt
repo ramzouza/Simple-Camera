76d1694317781adaff4da7febd0f43506f8dae87
ramzouza <ramezzaid@outlook.com>
2019-03-25 13:51:36 -0400

    added swiping espresso test #36

--- app/src/main/androidTest/com/simplemobiletools/camera/activities/MainActivityTest.kt
+++ app/src/main/androidTest/com/simplemobiletools/camera/activities/MainActivityTest.kt
@@ -5,6 +5,7 @@ import org.junit.Before
 import android.support.*
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeRight
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -43,35 +44,50 @@ class MainActivityTest {
         */
 
         //assert that the button is display
-        onView(withId(R.id.advanced_camera)).check(matches(isDisplayed()));
+        onView(withId(R.id.advanced_camera)).check(matches(isDisplayed()))
 
         //click on the button
-        onView(withId(R.id.advanced_camera)).perform(click());
+        onView(withId(R.id.advanced_camera)).perform(click())
 
 
         // Once the button advance camera is click, the smart hub appear with 4 buttons displayed
-        onView(withId(R.id.qr_code)).check(matches(isDisplayed()));
-        onView(withId(R.id.math_hub)).check(matches(isDisplayed()));
-        onView(withId(R.id.detect_object)).check(matches(isDisplayed()));
-        onView(withId(R.id.image_filter)).check(matches(isDisplayed()));
+        onView(withId(R.id.qr_code)).check(matches(isDisplayed()))
+        onView(withId(R.id.math_hub)).check(matches(isDisplayed()))
+        onView(withId(R.id.detect_object)).check(matches(isDisplayed()))
+        onView(withId(R.id.image_filter)).check(matches(isDisplayed()))
 
     }
 
     @Test
     fun detectObjectTest (){
-        onView(withId(R.id.advanced_camera)).check(matches(isDisplayed()));
+        onView(withId(R.id.advanced_camera)).check(matches(isDisplayed()))
 
         //click on the button
-        onView(withId(R.id.advanced_camera)).perform(click());
+        onView(withId(R.id.advanced_camera)).perform(click())
 
 
         // Once the button advance camera is click, the smart hub appear with 4 buttons displayed
-        onView(withId(R.id.detect_object)).check(matches(isDisplayed()));
+        onView(withId(R.id.detect_object)).check(matches(isDisplayed()))
 
         // Once button is clicked check thats it clicked idk
-        onView(withId(R.id.detect_object)).perform(click());
+        onView(withId(R.id.detect_object)).perform(click())
+
+    }
+
+    @Test
+    fun swipingTest(){
+
+        onView(withId(R.id.swipe)).perform(swipeRight())
+
+        // Once the button advance camera is click, the smart hub appear with 4 buttons displayed
+        onView(withId(R.id.qr_code)).check(matches(isDisplayed()))
+        onView(withId(R.id.math_hub)).check(matches(isDisplayed()))
+        onView(withId(R.id.detect_object)).check(matches(isDisplayed()))
+        onView(withId(R.id.image_filter)).check(matches(isDisplayed()))
 
     }
 
     
+
+
 }
