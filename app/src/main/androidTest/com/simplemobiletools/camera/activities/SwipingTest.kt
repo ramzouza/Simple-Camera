d3cb54b4703fc7ae016c097d1817463432275778
Calvin <b_calvin@hotmail.com>
2019-03-25 20:00:53 -0400

    Advanced UI Test for Swiping #36
    Co-authored-by: ramzouza <ramezzaid@outlook.com>

--- app/src/main/androidTest/com/simplemobiletools/camera/activities/SwipingTest.kt
+++ app/src/main/androidTest/com/simplemobiletools/camera/activities/SwipingTest.kt
@@ -56,19 +56,15 @@ class SwipingTest {
         onView(withId(R.id.math_hub)).check(matches(isDisplayed()))
         onView(withId(R.id.detect_object)).check(matches(isDisplayed()))
         onView(withId(R.id.image_filter)).check(matches(isDisplayed()))
-    }
 
-    @Test
-    fun enableSwipingUpDownTest(){
-        // ensure that swiping is working
         onView(withId(R.id.swipe)).perform(swipeRight())
-
-        // Once the button advance camera is click, the smart hub appear with 4 buttons displayed
         onView(withId(R.id.qr_code)).check(matches(isDisplayed()))
-        onView(withId(R.id.math_hub)).check(matches(isDisplayed()))
-        onView(withId(R.id.detect_object)).check(matches(isDisplayed()))
-        onView(withId(R.id.image_filter)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.swipe)).perform(swipeUp())
+        onView(withId(R.id.settings_swipe)).check(matches(isDisplayed()))
+        Espresso.pressBack()
     }
+
 
 
 
